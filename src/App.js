d4663f01b10884fccc6c4493f32bc573eb0ce88e
Mwininia-Saanyeh <ramoakidas@gmail.com>
2022-08-22 17:24:30 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ContactScreen from "./screens/ContactScreen";
+import AboutScreen from "./screens/AboutScreen";
+import DetailScreen from "./screens/DetailScreen";
+import Header from "./Components/Header";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <div>
+      <Header/>
+    <Routes>
+       <Route path="/" element={<HomeScreen/>}/>
+       <Route path="/contact" element={<ContactScreen/>}/>
+       <Route path="/about" element={<AboutScreen/>}/>
+       <Route path="/detail" element={<DetailScreen/>}/>
+    </Routes></div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
